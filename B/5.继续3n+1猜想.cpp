5cfd1d201de1770099d676f5bd17aedc0ab36a36
大脸 <18842556043@163.com>
2018-01-06 17:50:39 +0800

    5

---
+++ B/5.继续3n+1猜想.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int cmp(int a,int b) {return a > b;}
+
+int main(){
+	int n(0);
+	cin >> n;
+	int *a = new int[n];
+	for(int i=0;i<n;i++){
+		cin >> a[i];
+	}
+	int t;
+	sort(a,a+n,cmp);
+	for(int i=0;i<n;i++){
+		t=a[i];
+		while(t!=1 && t!=999){
+			if(t%2==0){
+				t=t/2;
+			}else{
+				t=(3*t+1)/2;
+			}
+			for(int j=0;j<n;j++){
+				if(t==a[j] && j!=i){
+					a[j]=999;
+				}
+			}
+		}
+	}
+	sort(a,a+n,cmp);
+	int temp(0);
+	for(int i=n-1;i>=0;i--){
+		if(a[i]!=999){
+			temp=i;
+		}
+	}
+	for(int i=temp;i<n-1;i++){
+		cout << a[i] << " ";
+	}
+	cout << a[n-1];
+	delete [] a;
+	return 0;
+}
